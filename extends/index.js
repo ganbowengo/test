dc8aa6e3abb319859fc401dfd331febdb4f7b709
ganbowengo <1245960973@qq.com>
2021-03-25 21:25:28 +0800

    extend

--- extends/index.js
+++ extends/index.js
@@ -3,7 +3,7 @@
  * @Author: ganbowen
  * @Date: 2020-01-07 10:43:25
  * @LastEditors  : ganbowen
- * @LastEditTime : 2020-01-10 16:09:49
+ * @LastEditTime : 2021-03-25 21:25:20
  */
 function Parent(name) {
     this.name = name
@@ -42,3 +42,31 @@ Son.see() // 继承类方法
 son.say() // 继承原型方法
 Son.cry()
 son.walk()
+
+
+// 继承
+function inherit(Parent, Child) {
+    // 继承父类的原型属性与方法
+    Child.prototype = Object.create(Parent.prototype)
+    // prototype.constructor 指向构造函数本身
+    Child.prototype.constructor = Child
+    // 存储超类
+    Child.super = Parent
+    // 子类构造函数的__proto__指向父类构造器，继承父类的静态方法  Son.__proto__ = Parent
+    if (Object.setPrototypeOf)   {
+        // setPrototypeOf es6
+        Child.setPrototypeOf(Child, Parent)  
+    } else if (Child.__proto__) {
+        // __proto__ es6 引入，但是部分浏览器早已支持
+        Child.__proto__ = Parent
+    } else {
+        // 兼容 IE10 等陈旧浏览器
+        // 将 Parent 上的静态属性和方法拷贝一份到 Child 上，不会覆盖 Child 上的方法
+        // 缺点： 属性性和方法的继承我们是静态拷贝的，继承完后续父类的改动不会自动同步到子类。
+        for (var k in Parent) {
+            if (Parent.hasOwnProperty(k) && !(k in Child)) {
+                Child[k] = Parent[k]
+            }
+        }
+    }
+}
