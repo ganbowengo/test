6c5c140d5932359a33bf4ebaa028540c92724f67
ganbowengo <1245960973@qq.com>
2021-04-07 21:39:24 +0800

    实现多个then方法调用

--- Promise/newPromise.js
+++ Promise/newPromise.js
@@ -2,9 +2,12 @@
  * @Author       : ganbowen
  * @Date         : 2021-04-07 21:10:39
  * @LastEditors  : ganbowen
- * @LastEditTime : 2021-04-07 21:32:13
+ * @LastEditTime : 2021-04-07 21:38:47
  * @Descripttion : promise node v12+
  */
+// 1. 实现同步版本
+// 2. 实现简易版异步版本
+// 3. 实现多个then方法调用
 const PENDING = 'pending'
 const FULFUILLED = 'fulfilled'
 const REJECTED = 'rejected'
@@ -13,8 +16,9 @@ class Promise1 {
     status = PENDING
     value = null
     reason = null
-    fulfillCallback = null
-    rejectedCallback = null
+    fulfillCallbacks = []
+    rejectedCallbacks = []
+
     constructor (executor) {
         executor(this.resolve, this.reject)
     }
@@ -23,7 +27,9 @@ class Promise1 {
         if (this.status === PENDING) {
             this.status = FULFUILLED
             this.value = value
-            this.fulfillCallback && this.fulfillCallback(value)
+            while(this.fulfillCallbacks.length) {
+                this.fulfillCallbacks.shift()(value)
+            }
         }
     }
 
@@ -31,7 +37,9 @@ class Promise1 {
         if (this.status === PENDING) {
             this.status = REJECTED
             this.reason = reason
-            this.rejectedCallback && this.rejectedCallback(reason)
+            while(this.rejectedCallbacks.length) {
+                this.rejectedCallbacks.shift()(reason)
+            }
         }
     }
 
@@ -41,8 +49,8 @@ class Promise1 {
         } else if (this.status === REJECTED) {
             rejectedFn(this.reason)
         } else if (this.status === PENDING) {
-            this.fulfillCallback = fulfillFn
-            this.rejectedCallback = rejectedFn
+            this.fulfillCallbacks.push(fulfillFn)
+            this.rejectedCallbacks.push(rejectedFn)
         }
     }
 
@@ -71,3 +79,25 @@ new Promise1((resolve, reject) => {
 }, reason => {
     console.log('reason', reason)
 })
+
+const promise = new Promise1((resolve, reject) => {
+    setTimeout(() => {
+      resolve('success')
+    }, 2000); 
+  })
+  
+  promise.then(value => {
+    console.log(1)
+    console.log('resolve', value)
+  })
+   
+  promise.then(value => {
+    console.log(2)
+    console.log('resolve', value)
+  })
+  
+  promise.then(value => {
+    console.log(3)
+    console.log('resolve', value)
+  })
+  
