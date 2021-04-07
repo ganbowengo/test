600baac749d1519148b99472853de8a24dc5ddcf
ganbowengo <1245960973@qq.com>
2021-04-07 21:32:31 +0800

    实现简易版异步promise

--- Promise/newPromise.js
+++ Promise/newPromise.js
@@ -2,7 +2,7 @@
  * @Author       : ganbowen
  * @Date         : 2021-04-07 21:10:39
  * @LastEditors  : ganbowen
- * @LastEditTime : 2021-04-07 21:26:09
+ * @LastEditTime : 2021-04-07 21:32:13
  * @Descripttion : promise node v12+
  */
 const PENDING = 'pending'
@@ -13,6 +13,8 @@ class Promise1 {
     status = PENDING
     value = null
     reason = null
+    fulfillCallback = null
+    rejectedCallback = null
     constructor (executor) {
         executor(this.resolve, this.reject)
     }
@@ -21,6 +23,7 @@ class Promise1 {
         if (this.status === PENDING) {
             this.status = FULFUILLED
             this.value = value
+            this.fulfillCallback && this.fulfillCallback(value)
         }
     }
 
@@ -28,6 +31,7 @@ class Promise1 {
         if (this.status === PENDING) {
             this.status = REJECTED
             this.reason = reason
+            this.rejectedCallback && this.rejectedCallback(reason)
         }
     }
 
@@ -36,6 +40,9 @@ class Promise1 {
             fulfillFn(this.value)
         } else if (this.status === REJECTED) {
             rejectedFn(this.reason)
+        } else if (this.status === PENDING) {
+            this.fulfillCallback = fulfillFn
+            this.rejectedCallback = rejectedFn
         }
     }
 
@@ -49,6 +56,16 @@ class Promise1 {
 new Promise1((resolve, reject) => {
     resolve('res')
     reject('err')
+}).then(res => {
+    console.log('res', res)
+}, reason => {
+    console.log('reason', reason)
+})
+
+new Promise1((resolve, reject) => {
+    setTimeout(() => {
+        resolve('setTimeout - res')
+    }, 1000)
 }).then(res => {
     console.log('res', res)
 }, reason => {
